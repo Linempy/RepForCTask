7b1397776993bd544014a1d6c457da205a20c03e
Linempy <mychinnik@yandex.ru>
2024-12-13 20:00:50 +0300

    second commit

---
+++ KR_2/KR_2.c
@@ -0,0 +1,34 @@
+﻿#include <stdio.h>
+
+
+#define TOTAL_MARKS		10
+
+int main()
+{
+	int nums[] = { -3, 0, 5, -8, 1, 10, 1 };
+	int minNum, t;
+	size_t pos;
+	size_t lengthNums = sizeof(nums) / sizeof(*nums);
+	
+	for (size_t i = 0; i < lengthNums-1; i++) {
+		minNum = nums[i + 1];
+		pos = i;
+		for (size_t j = i+1; j < lengthNums; j++) {
+			if (nums[pos] > nums[j]) {
+				minNum = nums[j];
+				pos = j;
+			}
+		}
+		if (pos != i) {
+			int t = nums[i];
+			nums[i] = nums[pos];
+			nums[pos] = t;
+		}
+	}
+
+	for (size_t i = 0; i < lengthNums; i++) {
+		printf("%d ", nums[i]);
+	}
+
+
+}
