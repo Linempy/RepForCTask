049fb45ee1c6f42c9d5ca54e21b6ba344de36b0a
Linempy <mychinnik@yandex.ru>
2024-12-13 19:00:14 +0300

    fisrt commit

---
+++ KR_2/KR_2.cpp
@@ -0,0 +1,24 @@
+﻿#include <stdio.h>
+
+
+int main()
+{
+	double arr[20] = { 0 };
+	size_t count = 0;
+	double summaNum = 0;
+	char c;
+
+	while (count < 20 && scanf_s("%lf", &arr[count]) == 1 && getchar() != '') {
+		summaNum += arr[count];
+		printf("%lf", arr[count]);
+		count++;
+	}
+
+	size_t lengthArr = sizeof(arr) / sizeof(arr[0]);
+	for (size_t i = 0; i < lengthArr; i++) {
+		if (arr[i]) count += 1;
+	}
+	printf("%.2lf", (double)summaNum / count);
+
+
+}
